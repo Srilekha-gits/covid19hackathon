33b04e4c0bcd0089d77474bddeb0363b708b0e7e
Srilekha-gits <56781872+Srilekha-gits@users.noreply.github.com>
2020-05-03 01:49:52 -0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,66 @@
+<?php include('header.php'); ?>
+
+
+
+
+
+	<!-- this is for donor registraton -->
+	<div class="main_content" style="background-color:#fff;">
+		<h3 class="text-center" style="background-color:#272327;color: #fff;">About Us</h3>
+		<div class="col-md-12">
+			<div class="col-md-6">
+				<img src="img/aboutimag33.jpg" alt="" class="img-responsive">
+			</div>
+
+			<div class="col-md-6">
+				<article>
+					<h3 style="color:#0616BC;">We are Beside you</h3>
+					<p class="text-justify">We hospitals chain together to manage their patient bookings through efficient doctor appointment booking apps.</p>
+					<h3 style="color:#0616BC;">24/7 hour service</h3>
+					<p class="text-justify">We provide 24 hrs service to take care patient and reduce the severity of covid-19 among patients. We take immediate treatment for patients who gets the symptoms of covid.</p>
+				</article>
+			</div>
+		</div>
+
+
+		<div class="col-md-12">
+			<div class="col-md-6">
+				<article>
+					<h3 style="color:#0616BC;">Let's Take a Survey</h3>
+					<p class="text-justify"><ol>1. Do you have cough.</ol>
+					<ol>2. Do you have fever.</ol>
+					<ol>3. Have you vistied any international places 30days before.</ol>
+					<ol>4. If any symptoms you have consult doctor by getting appointment.</ol>
+					<b>We are here to help you!!!</b>
+				</article>
+			</div>
+			<div class="col-md-6">
+				<img src="img/aboutimag11.jpg" alt="" class="img-responsive"><br>
+			</div>
+		</div>
+          
+    </div>
+		
+	
+	
+	
+
+	
+ <?php include('footer.php'); ?>
+
+
+	
+	</div><!--  containerFluid Ends -->
+
+
+
+
+	<script src="js/bootstrap.min.js"></script>
+
+
+
+
+
+	
+</body>
+</html>
---
+++ contactuss.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Medical Management System</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+	<style>
+		.error {color: #FF0000;}
+	</style>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="header_top">
+			<!-- <a href="index.php"><img src="img/dash2.jpg" alt="logo.png img"></a> -->
+
+			<span style="font-size:50px;color:#2c2f84;font-weight:bolder;margin-left:15px;">Doctor Appoinment System</span>
+		</div>
+
+	<!-- 	this is for menu -->
+	<div class="navbar navbar-default nav">
+		<nav class="menu">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="medical_college.php">Medical College</a></li>
+				<li><a href="doctorinfo.php">Medical Doctors</a></li>
+				<li><a href="about.php">About Us</a></li>
+				<li><a href="contactus.php">Contact Us</a></li>
+				<li><a href="signin.php">User Login</a></li>
+				
+			</ul>
+		</nav>
+	</div>
+	
+
+
+
+
+	<!-- this is for donor registraton -->
+	<div class="contactus"  style="background-color:#fff;">
+		<h3 class="text-center" style="background-color:#272327;color: #fff;">Contact Us</h3>
+
+		
+		
+		<div class="main_content">
+			<div class="col-md-6" style="border-right: 2px solid black;">
+				<article>
+					<h2>Mr.Faizal <h2><h4>Studied B.Sc in Electronics and Communication Engineering</h4>
+							<h5>Hajee Mohammad Danesh Science and Technology University, Chittor-517001.</h5>
+						<p> https://portfolio.developerazad.com <br>
+							https://developerazad.wordpress.com <br>
+							https://linkedin.com/in/developerazad <br>
+							https://facebook/developerazad <br>
+							cell: +91 9176476199</p><br>
+				</article>
+			</div>
+			<div class="col-md-6">
+				<h2 class="text-center">Your Message</h2>
+				<form action="" method="post" class="text-center">
+						<label>
+								First Name: <input type="text" name="firstname" value="" placeholder="firstname" required>
+						</label><br><br>
+
+						<label>
+								Last Name: <input type="text" name="lastname" value="" placeholder="lastname" required>
+						</label><br><br>	
+
+						<label>
+								Email: <input type="email" name="email"  value="" placeholder="Your email" required>
+						</label><br><br>
+						<label>
+								Your Comment: <textarea name="comment" id="" cols="30" rows="4" required></textarea> 
+						</label><br><br>
+								
+						<input type="submit" value="Send Us" name="submit" style="margin-top: 75px;border-radius: 2px;"/>
+						<!-- <button name="submit" type="submit" class="btn btn-info"><i class="icon-signin icon-large"></i>&nbsp;Sign Up</button>
+ -->
+					</form><br><br><br>
+			</div>
+
+          
+ 		</div>
+
+	</div>
+	
+	
+
+	
+    <!-- footer section --> 
+			<footer>
+				<div class="col-md-12 fsection">
+					<div class="col-md-4" id="contact">
+					<p>Category</p> <hr>
+					<ul>
+						<li><a href="signin.php">Search for Doctors</a></li>
+						<li><a href="signin.php">Contact with Doctors</a></li>
+						
+					</ul>
+					</div>
+					<div class="col-md-4">
+						<p>Contact</p> <hr>
+						<p>Mr.Faizal <br>
+							faizal@gmail.com <br>
+							Cell: 9738698242</p>
+							<span style="color: red;font-size: 15px">&copy;2020-All Rights Reserved</span>
+					</div>
+					<div class="col-md-4 share_img">
+					<p>Link With</p> <hr>
+						<a href="" ><img src="img/fb-free.png" alt="facebook"></a>
+						<a href=""><img src="img/gogle-plud-free.png" alt="google-plus"></a>
+						<a href=""><img src="img/twitter.png" alt="twitter"></a>
+						
+					</div>
+				</div>
+				
+
+			</footer>
+
+		<!-- footer section Ends--> 
+
+
+	
+	</div><!--  containerFluid Ends -->
+
+
+
+
+	<script src="js/bootstrap.min.js"></script>
+
+
+ 
+<!-- contact information inserting -->
+												 
+
+
+
+	
+</body>
+</html>
+
